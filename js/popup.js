0de186028d261b2c999abd724dadf45e1de4f323
eresha1 <eresha39@gmail.com>
2018-07-01 20:04:17 +0700

    Анимация формы поиска

---
+++ js/popup.js
@@ -0,0 +1,39 @@
+
+var link = document.querySelector(".button-search");
+var searchForm = document.querySelector(".search-info");
+var dateArrive = searchForm.querySelector("#arrive");
+var form = document.querySelector("form");
+var dateDepart = searchForm.querySelector("#depart");
+var numberAdult = searchForm.querySelector("#adult");
+var numberChildren = searchForm.querySelector("#children");
+
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  searchForm.classList.toggle("modal-show");
+  searchForm.classList.remove("modal-error");
+
+  dateArrive.focus();
+
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!dateArrive.value || !dateDepart.value ||
+    !numberAdult.value || !numberChildren.value) {
+    evt.preventDefault();
+    searchForm.classList.remove("modal-error");
+    searchForm.offsetWidth = searchForm.offsetWidth;
+    searchForm.classList.add("modal-error");
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (event.keyCode === 27) {
+
+    if (searchForm.classList.contains("modal-show")) {
+      event.preventDefault();
+      searchForm.classList.remove("modal-show");
+      searchForm.classList.remove("modal-error");
+    }
+  }
+});
